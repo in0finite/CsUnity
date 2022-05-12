8ed32239d868bf01e0ccbe450ebce16885951733
in0finite <leshow816@gmail.com>
2022-05-12 15:13:40 +0200

    add FpsLimiter

---
+++ Assets/CsUnity/FpsLimiter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CsUnity
+{
+    public class FpsLimiter : MonoBehaviour
+    {
+        public int fpsLimit = 60;
+
+        void Start()
+        {
+            Application.targetFrameRate = this.fpsLimit;
+        }
+    }
+}
