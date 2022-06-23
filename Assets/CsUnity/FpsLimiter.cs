411dd61b5d04fca300b7c39215635534bcef417a
in0finite <leshow816@gmail.com>
2022-06-23 23:16:35 +0200

    limit fps in edit mode

--- Assets/CsUnity/FpsLimiter.cs
+++ Assets/CsUnity/FpsLimiter.cs
@@ -1,21 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UGameCore.Utilities.Editor;
 
 namespace CsUnity
 {
     public class FpsLimiter : MonoBehaviour
     {
-        public int fpsLimit = 60;
+        public ushort fpsLimit = 60;
+
+
+        static FpsLimiter()
+        {
+            SetEditModeLimit(30);
+        }
 
         void Start()
         {
             Application.targetFrameRate = this.fpsLimit;
+            SetEditModeLimit(this.fpsLimit);
         }
 
         void OnValidate()
         {
             Application.targetFrameRate = this.fpsLimit;
+            SetEditModeLimit(this.fpsLimit);
+        }
+
+        static void SetEditModeLimit(ushort value)
+        {
+            FpsLimiterForEditMode.IsEnabled = true;
+            FpsLimiterForEditMode.TargetFps = value;
         }
     }
 }
